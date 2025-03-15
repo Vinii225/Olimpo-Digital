cddfb8c00416c749d516bd1e0762d459ea83453d
LuisEduardoX <eduardogomessoares11m@gmail.com>
2025-03-15 01:21:19 -0300

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+let lastScrollY = window.scrollY;
+let scrolledToSection = false; 
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navegacao");
+
+    
+    if (!scrolledToSection) {
+        if (window.scrollY < lastScrollY) {
+            navbar.classList.add("visible");
+            navbar.classList.remove("hidden");
+        } else {
+            navbar.classList.add("hidden");
+            navbar.classList.remove("visible");
+        }
+    }
+
+    lastScrollY = window.scrollY;
+});
+
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", (event) => {
+        scrolledToSection = true;
+        const targetId = event.target.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            setTimeout(() => {
+                scrolledToSection = false;
+            }, 1000);
+        }
+    });
+});
